de2301ea7f086da73aee8e2841c77e1841eae61b
Thomas RAMAROMANANATOANDRO <thomas.ramaro@gmail.com>
2021-03-13 14:00:36 +0100

    Reponse 4.1.1 Pattern Builder Personnel-tests passed

--- src/main/java/fr/uvsq/poo/compte/Personnel.java
+++ src/main/java/fr/uvsq/poo/compte/Personnel.java
@@ -1,5 +1,95 @@
 package fr.uvsq.poo.compte;
 
-public class Personnel {
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
+public class Personnel {
+  public static LocalDate DATE_NAISSANCE = LocalDate.of(2000, 1, 1);
+  
+  private final String nom;
+  private final String prenom;
+  private final List<String> fonctions;
+  private final List<String> numeros;
+  private LocalDate dateNaissance;
+  
+  
+     private Personnel(Builder builder) {
+	    this.nom = builder.nom;
+	    this.prenom = builder.prenom;
+	    this.fonctions = builder.fonctions;
+	    this.numeros = builder.numeros;
+	    this.dateNaissance = builder.dateNaissance;
+	  }
+  
+      public String getNom() {
+	    return this.nom;
+	  }
+
+	  public String getPrenom() {
+	    return this.prenom;
+	  }
+	  
+	  public String getDescription() {
+		  return this.prenom + ' '+this.nom;
+	  }
+
+	  public List<String> getFonctions() {
+	    return this.fonctions;
+	  }
+	  
+	  public List<String> getNumeros(){
+		  return this.numeros;
+	  }
+
+	  public LocalDate getDateNaissance() {
+	    return this.dateNaissance;
+	  }
+
+	  @Override
+	  public String toString() {
+	    return "Personnel{" +
+	            "Nom='" + this.nom + '\'' +
+	            ", Prénom='" + this.prenom + '\'' +
+	            '}';
+	  }
+  
+  public static class Builder{
+	  private final String nom;
+	  private final String prenom;
+	  private final List<String> fonctions;
+	  private final List<String> numeros;
+	  private LocalDate dateNaissance;
+	  
+	  public Builder(String nom,String prenom) {
+		  this.nom = nom;
+		  this.prenom = prenom;
+		  
+		  this.fonctions = new ArrayList<>();
+		  this.numeros = new ArrayList<>();
+		  this.dateNaissance = DATE_NAISSANCE;
+	}
+	 
+	  public Personnel build() {
+	      return new Personnel(this);
+	    }
+
+	    public Builder ajouterFonction(String fonction) {
+	      fonctions.add(fonction);
+	      return this;
+	    }
+	    
+	    public Builder ajouterNumeros(String numero) {
+	    	numeros.add(numero);
+	    	return this;
+	    }
+	    
+
+	    public Builder dateNaissance(int annee, int mois, int jour) {
+	      dateNaissance = LocalDate.of(annee, mois, jour);
+	      return this;
+	    }
+	  
+	  
+  }
 }
--- src/test/java/fr/uvsq/poo/compte/PersonnelTest.java
+++ src/test/java/fr/uvsq/poo/compte/PersonnelTest.java
@@ -2,13 +2,61 @@ package fr.uvsq.poo.compte;
 
 import static org.junit.Assert.*;
 
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class PersonnelTest {
+	private Personnel luke;
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+	  @Before
+	  public void setup() {
+	    luke = new Personnel.Builder("Skywalker","Luke")
+	            .dateNaissance(2007, 10, 4)
+	            .build();
+	  }
+
+	  @Test
+	  public void creerPersonnelAvecNomPrenomTest() {
+	    Personnel p = new Personnel.Builder("Skywalker","Luke").build();
+
+	    assertPersonnel(p, "Skywalker", "Luke", Collections.emptyList(), 2000, 1, 1);
+	  }
+
+	  @Test
+	  public void creerPersonnelAvecNomPrenomFonctionTest() {
+	    Personnel p = new Personnel.Builder("Skywalker","Luke")
+	      .ajouterFonction("Chargé de mission")
+	      .ajouterFonction("Directeur")
+	      .build();
+
+	    assertPersonnel(p, "Skywalker","Luke", Arrays.asList("Chargé de mission", "Directeur"), 2000, 1, 1);
+	  }
+
+	  @Test
+	  public void creerPersonnelAvecDateNaissance() {
+	    assertPersonnel(luke, "Skywalker","Luke", Collections.emptyList(), 2007, 10, 4);
+	  }
+
+	  @Test
+	  public void creerChaineCaracterePersonnelValideTest() {
+	    assertEquals("Personnel{Nom='Skywalker', Prénom='Luke'}", luke.toString());
+	  }
+
+	  @Test
+	  public void creerUneDescriptionPersonnelTest() {
+	    assertEquals("Luke Skywalker", luke.getDescription());
+	  }
+
+	  private static void assertPersonnel(Personnel p, String nom, String prenom, List<String> fonctions, int annee, int mois, int jour) {
+	    assertEquals(nom, p.getNom());
+	    assertEquals(prenom, p.getPrenom());
+	    assertEquals(fonctions, p.getFonctions());
+	    assertEquals(LocalDate.of(annee, mois, jour), p.getDateNaissance());
+	  }
 
 }
